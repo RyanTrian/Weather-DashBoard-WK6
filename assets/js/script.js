c89c069438146fce10dd112f59890a607eec5aeb
Minh Truong <mptruong49@gmail.com>
2022-10-06 17:38:22 -0600

    fetch geo location

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
-var APIkey = '55646f4610124c7421dc7f636430ed78';
+// Global variables
+var APIkey = "&appid=55646f4610124c7421dc7f636430ed78";
+var searchInput = $("#search-input");
+var submitBtn = $("#submit-btn");
+var citiesList = $(".previous-cities");
+var currentWeather = $(".current-weather");
+var forecast5 = $(".five-days");
+
+//
+
+function showWeather() {
+  // get the city's name, store it in a variable
+  let cityName = searchInput.val();
+  searchInput.val("");
+  // fetch lat and lon
+  let geoCodingAPI =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + APIkey;
+
+  function currentWeather(data) {
+    let la = data[0].lat;
+    let lon = data[0].lon;
+  }
+  function forecast5(data) {
+    
+  }
+
+  fetch(geoCodingAPI)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      currentWeather(data);
+      forecast5(data);
+    });
+}
+
+submitBtn.on("click", showWeather);
